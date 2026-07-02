3be04130d8915c559e7ea92e7024aaf60467206e
LG M <lgm@Mac.attlocal.net>
2026-07-02 08:39:08 -0700

    Make bid tool feel like an app

---
+++ tests/platform-bid-app.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const html = fs.readFileSync(path.join(__dirname, '..', 'platform.html'), 'utf8');
+
+function contains(snippet) {
+  assert(
+    html.includes(snippet),
+    `Expected platform.html to contain: ${snippet}`
+  );
+}
+
+contains('class="bid-app-shell no-print"');
+contains('id="app-project-title"');
+contains('onclick="newProjectFolder()"');
+contains('onclick="saveBidDraft()"');
+contains('onclick="acceptBid()"');
+contains('onclick="printBidsAndInvoicing()"');
+contains('data-flow-step="setup"');
+contains('function renderBidAppShell(t)');
+contains('renderBidAppShell(t)');
